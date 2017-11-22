5c83cb1e8f62764e21d80c1294fa3b20b33c642b
Fabien Le Bronnec <fabien.lebronnec@compta.com>
2017-11-22 10:31:00 +0100

    Gestion tetriminos

--- src/fr/tetris/dao/TetriminoApplicationDAO.java
+++ src/fr/tetris/dao/TetriminoApplicationDAO.java
@@ -1,5 +1,46 @@
 package fr.tetris.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import fr.tetris.model.Tetrimino;
+
 public class TetriminoApplicationDAO {
+	
+	private HashMap<String, Tetrimino> tetriminos = new HashMap<>();
+	
+	public TetriminoApplicationDAO() {
+		// TODO Auto-generated constructor stub
+		Tetrimino tetrimino = new Tetrimino();
+		tetrimino.setNom("1");
+		tetrimino.setCouleur("136");
+		save(tetrimino);
+		tetrimino = new Tetrimino();
+		tetrimino.setNom("25456");
+		tetrimino.setCouleur("136456546");
+		save(tetrimino);
+	}
+	
+	public Tetrimino get(String id) {
+		return tetriminos.get(id);
+	}
+	
+	public List<Tetrimino> findAll() {
+		return new ArrayList<>(tetriminos.values());
+	}
+	
+	public Tetrimino save(Tetrimino tetrimino) {
+		if (tetrimino.getId() == null) {
+			tetrimino.setId(UUID.randomUUID().toString());
+		}
+		tetriminos.put(tetrimino.getId(), tetrimino);
+		return tetrimino;
+	}
+	
+	public void delete(Tetrimino tetrimino) {
+		tetriminos.remove(tetrimino.getId());
+	}
 
 }
--- src/fr/tetris/model/Tetrimino.java
+++ src/fr/tetris/model/Tetrimino.java
@@ -2,9 +2,19 @@ package fr.tetris.model;
 
 public class Tetrimino {
 	
+	private String id;
+	
 	private String nom;
 	
 	private String couleur;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getNom() {
 		return nom;
