31612d5d40eb0da09d0772fbc3145f9e13511415
Thymote <thymote.morin@gmail.com>
2017-11-22 10:52:22 +0100

    Login + home

--- src/fr/tetris/filter/SessionFilter.java
+++ src/fr/tetris/filter/SessionFilter.java
@@ -1,5 +1,41 @@
 package fr.tetris.filter;
+import java.io.IOException;
 
-public class SessionFilter {
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebFilter("/*")
+public class SessionFilter implements Filter {
+	
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)throws IOException, ServletException {
+		
+			HttpServletRequest request = (HttpServletRequest) req;
+			HttpServletResponse response = (HttpServletResponse) resp;
+			if((request.getSession().getAttribute("username")== null || request.getSession().getAttribute("username")== "") && !request.getRequestURI().contains("/login")){
+				request.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+			}
+			else {
+				chain.doFilter(req, resp);
+			}
+			
+		}
+		
+		@Override
+		public void init(FilterConfig filterConfig) throws ServletException {
+			// TODO Auto-generated method stub
+			Filter.super.init(filterConfig);
+		}
+		
+		@Override
+		public void destroy() {
+			// TODO Auto-generated method stub
+			Filter.super.destroy();
+		}
 }
--- src/fr/tetris/servlet/LoginServlet.java
+++ src/fr/tetris/servlet/LoginServlet.java
@@ -1,5 +1,41 @@
 package fr.tetris.servlet;
 
-public class LoginServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/WEB-INF/views/login.jsp").forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String username = request.getParameter("username");
+		 String password = request.getParameter("motdepasse");
+		 HttpSession session = request.getSession();
+		 session.setAttribute("username", username );
+		 session.setAttribute("password",password);
+		// this.getServletContext().getRequestDispatcher("/home").forward( request, response );
+		 response.sendRedirect( request.getContextPath() + "/home");
+		
+	}
 
 }
